7b0c7415ace312aaf5ecdc628ed37fa6c57c288a
machism0 <benmiller1315@gmail.com>
2019-06-05 13:40:21 +0200

    fixed dihedral script

--- e05/dihedral_angles.sh
+++ e05/dihedral_angles.sh
@@ -9,6 +9,19 @@ echo $(ls)
 # ======= SET YOUR GROMACS SOURCE =======
 #source /usr/local/gromacs/bin/GMXRC
 source /home/mi/milleb92/mystorage/gromacs/bin/GMXRC
-gmx chi -f trajoutAref.xtc -s A.tpr -maxchi 4 -phi -psi -o anglesA.xvg
-# gmx chi -f trajoutBref.xtc -s B.tpr -maxchi 4 -phi -psi -o anglesB.xvg
-rm *chi* # clean up the extra crap that gets made
+# DO A
+gmx chi -f trajoutAref.xtc -s A.tpr -maxchi 4 -phi -psi -all -o orderA.xvg -g chiA.log
+mkdir anglesA
+mv orderA.xvg anglesA/
+mv psi* anglesA/
+mv phi* anglesA/
+mv chi* anglesA/
+mv histo* anglesA/
+# DO B
+gmx chi -f trajoutBref.xtc -s B.tpr -maxchi 4 -phi -psi -all -o orderB.xvg -g chiB.log
+mkdir anglesB
+mv orderB.xvg anglesB/
+mv psi* anglesB/
+mv phi* anglesB/
+mv chi* anglesB/
+mv histo* anglesB/
